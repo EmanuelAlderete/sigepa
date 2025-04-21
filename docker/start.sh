3a1602612134f09cc1a271920072b00e5d7cefe0
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-21 17:54:16 -0300

    refactor(docker): update docker-compose configuration and modify permissions for .gitignore files

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,10 @@ echo "Aguardando o banco de dados..."
 sleep 10
 
 # Roda migrações e seeders com --force para ambientes não interativos
+<<<<<<< HEAD
+=======
+php artisan key:generate
+>>>>>>> 42e4c50 (refactor(docker): update docker-compose configuration and modify permissions for .gitignore files)
 php artisan migrate --force
 php artisan db:seed --force
 
